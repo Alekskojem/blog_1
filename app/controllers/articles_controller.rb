93c1253d4240908215f67d657217877a091a0d8c
Aleksandr Kojemyako <alekskojem@gmail.com>
2018-05-07 15:52:22 +0300

    Implement articles#Remove

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,12 @@ def edit
   @article = Article.find(params[:id])
 end
 
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  redirect_to articles_path
+end
+
 def update
   @article = Article.find(params[:id])
    if @article.update(article_params)
