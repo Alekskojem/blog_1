d6e0f12d076c0ecdad0033d16dfd3b667864df45
Aleksandr Kojemyako <alekskojem@gmail.com>
2018-05-10 17:30:32 +0300

    Repair misstake

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get 'terms' => 'pages#terms'
 get 'about' => 'pages#about'
     resource :contacts, only: [:new, :create], path_names: {:new => ''}
     resources :articles do
-      resources :comments
+    resources :comments
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
