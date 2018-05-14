4d5b10817c7d2f23c91e8f0dcb483c2309a88b57
Aleksandr Kojemyako <alekskojem@gmail.com>
2018-05-14 14:48:06 +0300

    Reconfig article_controoler.rb file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,24 +27,27 @@ def edit
   @article = Article.find(params[:id])
 end
 
+def update
+   @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+       redirect_to @article
+   else
+    render action: 'edit'
+end
+end
+
 def destroy
   @article = Article.find(params[:id])
   @article.destroy
-  redirect_to articles_path
-end
-
-def update
-  @article = Article.find(params[:id])
-   if @article.update(article_params)
-       redirect_to @article
-  else
-    render action: 'edit'
+   
+  redirect_to article_path
   end
-end
+  
   private
 
   def article_params
     params.require(:article).permit(:title, :text)
  end
-
 end
+
